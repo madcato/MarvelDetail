0e20ee776d5f5700f9bbe9916ddba59be2f9a4f7
Daniel Vela <veladan@me.com>
2021-04-14 08:49:57 +0200

    Initial Commit

---
+++ MarvelDetail/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MarvelDetail
+//
+//  Created by Daniel Vela on 14/4/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
